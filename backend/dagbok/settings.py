59d794870761c73a6cc46ba40d697cc39eae4ba2
Adriano Alves Dal Cin Costa <adriano@agiliza.top>
2025-03-13 08:53:36 -0300

    Add Zappa output and logs to .gitignore- Add Zappa output directory to .gitignore

--- backend/dagbok/settings.py
+++ backend/dagbok/settings.py
@@ -87,26 +87,11 @@ WSGI_APPLICATION = 'dagbok.wsgi.application'
 
 
 # Database
-# https://docs.djangoproject.com/en/5.1/ref/settings/#databases
-
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.dummy'
     }
 }
-
-# Para usar um banco de dados gerenciado pela AWS (opcional):
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.postgresql',
-#         'NAME': os.getenv('DB_NAME'),
-#         'USER': os.getenv('DB_USER'),
-#         'PASSWORD': os.getenv('DB_PASSWORD'),
-#         'HOST': os.getenv('DB_HOST'),
-#         'PORT': os.getenv('DB_PORT', '5432'),
-#     }
-# }
 
 
 # Password validation
