1184192b90ee4e85431eee25bc99080b9bb1ea91
Adriano Alves Dal Cin Costa <adriano@agiliza.top>
2025-03-23 11:31:07 -0300

    Add JWT authentication and Nginx setup for Docker

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -1,8 +1,14 @@
 from django.urls import path
 from .views import GoogleLogin, ResumeAPIView, SocialTokenLoginView
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView, TokenVerifyView
 
 urlpatterns = [
     path('auth/google/', GoogleLogin.as_view(), name='google_login'),
     path('resume/', ResumeAPIView.as_view(), name='resume'),
     path('auth/token_login/<str:provider>/', SocialTokenLoginView.as_view(), name='social_token_login'),
+    
+    # JWT Auth
+    path('token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    path('token/verify/', TokenVerifyView.as_view(), name='token_verify'),
 ]
